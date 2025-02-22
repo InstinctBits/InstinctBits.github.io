f0afdb99e022390a1f06b6b010f79cf875245114
sacredabhishek <3474686+sacredabhishek@users.noreply.github.com>
2025-02-22 20:53:37 +0530

    made home page changes -2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>How we deployed this Simple Site
+    <a href="https://www.youtube.com/watch?v=vmqGPEeFcnE" target="_blank" rel="noopener">You Tube</a> +
+     <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h1>
   </div>
 </template>
 
