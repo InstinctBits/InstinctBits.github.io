9f485332d00450e167b688d4a5d42594a07a2412
sacredabhishek <3474686+sacredabhishek@users.noreply.github.com>
2025-02-22 21:07:55 +0530

    add about us details

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,38 @@
 <template>
   <div class="about">
-    <h1>How we deployed this Simple Site
-    <a href="https://www.youtube.com/watch?v=vmqGPEeFcnE" target="_blank" rel="noopener">You Tube</a> +
-     <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h1>
+    <h1>How we deployed this Simple Site with 
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>
+     </h1>
+     <h3> Refer to the following video for more information:
+       <a href="https://www.youtube.com/watch?v=vmqGPEeFcnE" target="_blank" rel="noopener">You Tube</a>
+      </h3> 
+          <!-- <h3>
+   
+      We used the following commands to deploy this site:
+      
+      sudo npm init vue@latest . 
+      npm install
+      npm run dev
+      git status
+      git add .
+      git commit -m "inital commit"
+      git push
+      gh auth login
+      sudo apt  install gh
+      gh auth login
+      gh repo view --web
+      npm install netlify-cli -g
+      sudo npm install netlify-cli -g
+      netlify init
+      git status
+      git add .
+      git commit -m "added netlify config"
+      git push
+      netlify open:site
+      
+   
+  </h3> -->
   </div>
 </template>
 
