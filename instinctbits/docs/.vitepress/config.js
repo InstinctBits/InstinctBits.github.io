21e38cdea00811e2a4ca434ec19daa603ececd18
sacredabhishek <3474686+sacredabhishek@users.noreply.github.com>
2025-02-22 18:38:00 +0530

    initial commit

--- instinctbits/docs/.vitepress/config.js
+++ instinctbits/docs/.vitepress/config.js
@@ -1,19 +1,32 @@
+// export default {
+//     title: 'DeepSeek',
+//     description: 'Your free all-in-one AI tool',
+//     themeConfig: {
+//       nav: [
+//         { text: 'Home', link: '/' },
+//         { text: 'Get App', link: '/get-app' }
+//       ],
+//       sidebar: [
+//         {
+//           text: 'Guide',
+//           items: [
+//             { text: 'Start Now', link: '/start-now' },
+//             { text: 'Get DeepSeek App', link: '/get-app' }
+//           ]
+//         }
+//       ]
+//     }
+//   }
+
 export default {
-    title: 'DeepSeek',
-    description: 'Your free all-in-one AI tool',
-    themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Get App', link: '/get-app' }
-      ],
-      sidebar: [
-        {
-          text: 'Guide',
-          items: [
-            { text: 'Start Now', link: '/start-now' },
-            { text: 'Get DeepSeek App', link: '/get-app' }
-          ]
-        }
-      ]
-    }
-  }
+  title: "VitePress",
+  description: "Vite & Vue Powered Static Site Generator",
+  themeConfig: {
+    nav: [
+      { text: "Guide", link: "/guide/" },
+      { text: "Reference", link: "/reference/" },
+      { text: "GitHub", link: "https://github.com/vuejs/vitepress" },
+    ],
+    socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
+  },
+};
